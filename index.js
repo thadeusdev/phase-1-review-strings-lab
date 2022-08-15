e129bd2177d44e2ac47dd7c5862579efe9963ea7
thadeus <thadeusnyariki94@gmail.com>
2022-08-15 14:32:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+/*
+index.js
+    currentUser
+      ✓ is defined
+    welcomeMessage
+      ✓ contains "Welcome to Flatbook, "
+      ✓ contains the value of the 'currentUser' variable
+      ✓ ends with an exclamation point!
+    excitedWelcomeMessage
+      ✓ contains "WELCOME TO FLATBOOK, "
+      ✓ contains the value of the 'currentUser' variable
+      ✓ ends with an exclamation point
+    shortGreeting
+      ✓ contains "Welcome, "
+      ✓ contains the first initial of the name stored in the 'currentUser' variable
+      ✓ ends with an exclamation point
+      */
+const currentUser = 'Grace Hoper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.substring(0, 1)}!`;
